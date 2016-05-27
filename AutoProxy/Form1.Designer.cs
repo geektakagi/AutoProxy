f7c4ed429e25732975cc991b408d5dcfa56a686e
renetbutler <Renet.pc@gmail.com>
2016-05-27 16:19:05 +0900

    resident program

--- AutoProxy/Form1.Designer.cs
+++ AutoProxy/Form1.Designer.cs
@@ -28,9 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoProxySetting));
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.btnProxyEnable = new System.Windows.Forms.Button();
             this.btnProxyDisable = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoProxyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -49,7 +60,7 @@
             // 
             this.btnProxyEnable.Location = new System.Drawing.Point(238, 270);
             this.btnProxyEnable.Name = "btnProxyEnable";
-            this.btnProxyEnable.Size = new System.Drawing.Size(75, 23);
+            this.btnProxyEnable.Size = new System.Drawing.Size(75, 30);
             this.btnProxyEnable.TabIndex = 1;
             this.btnProxyEnable.Text = "Enable";
             this.btnProxyEnable.UseVisualStyleBackColor = true;
@@ -59,11 +70,69 @@
             // 
             this.btnProxyDisable.Location = new System.Drawing.Point(471, 270);
             this.btnProxyDisable.Name = "btnProxyDisable";
-            this.btnProxyDisable.Size = new System.Drawing.Size(75, 23);
+            this.btnProxyDisable.Size = new System.Drawing.Size(75, 30);
             this.btnProxyDisable.TabIndex = 2;
             this.btnProxyDisable.Text = "Disable";
             this.btnProxyDisable.UseVisualStyleBackColor = true;
             this.btnProxyDisable.Click += new System.EventHandler(this.btnProxyDisable_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingToolStripMenuItem,
+            this.autoProxyToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 100);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(180, 30);
+            this.settingToolStripMenuItem.Text = "Setting";
+            this.settingToolStripMenuItem.Click += new System.EventHandler(this.settingToolStripMenuItem_Click);
+            // 
+            // autoProxyToolStripMenuItem
+            // 
+            this.autoProxyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enableToolStripMenuItem,
+            this.disableToolStripMenuItem});
+            this.autoProxyToolStripMenuItem.Name = "autoProxyToolStripMenuItem";
+            this.autoProxyToolStripMenuItem.Size = new System.Drawing.Size(180, 30);
+            this.autoProxyToolStripMenuItem.Text = "AutoProxy";
+            // 
+            // enableToolStripMenuItem
+            // 
+            this.enableToolStripMenuItem.Name = "enableToolStripMenuItem";
+            this.enableToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.enableToolStripMenuItem.Text = "Enable";
+            // 
+            // disableToolStripMenuItem
+            // 
+            this.disableToolStripMenuItem.Name = "disableToolStripMenuItem";
+            this.disableToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.disableToolStripMenuItem.Text = "Disable";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // AutoProxySetting
             // 
@@ -76,6 +145,7 @@
             this.MaximizeBox = false;
             this.Name = "AutoProxySetting";
             this.Text = "AutoProxySetting";
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +156,14 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button btnProxyEnable;
         private System.Windows.Forms.Button btnProxyDisable;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoProxyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- AutoProxy/Form1.cs
+++ AutoProxy/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,8 +14,27 @@ namespace AutoProxy
 {
     public partial class AutoProxySetting : Form
     {
+
         public AutoProxySetting() {
             InitializeComponent();
+            InTasktray();
+
+            System.Net.NetworkInformation.NetworkChange.NetworkAvailabilityChanged +=
+                new System.Net.NetworkInformation.NetworkAvailabilityChangedEventHandler(
+                NetworkChange_NetworkAvailabilityChanged);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_SYSCOMMAND = 0x112;
+            const long SC_CLOSE = 0xF060L;
+
+            if (m.Msg == WM_SYSCOMMAND && (m.WParam.ToInt64() & 0xFFF0L) == SC_CLOSE) {
+                InTasktray();
+                return;
+            }
+
+            base.WndProc(ref m);
         }
 
         private void ProxyEnable(object sender, EventArgs e) {
@@ -24,8 +45,7 @@ namespace AutoProxy
             SetReg_ProxyEnable(false);
         }
 
-        private Boolean SetReg_ProxyEnable(Boolean flag)
-        {
+        private Boolean SetReg_ProxyEnable(Boolean flag) {
             const int ProxyEnable   = 1;
             const int ProxyDisable  = 0;
 
@@ -39,8 +59,7 @@ namespace AutoProxy
             return true;
         }
 
-        private Boolean SetReg_ProxyServer(String ServerAddr)
-        {
+        private Boolean SetReg_ProxyServer(String ServerAddr) {
             Microsoft.Win32.RegistryKey regkey =
                 Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings", true);
             regkey.SetValue("ProxyServer", ServerAddr, Microsoft.Win32.RegistryValueKind.String);
@@ -49,14 +68,57 @@ namespace AutoProxy
             return true;
         }
 
-        private Boolean SetReg_ProxyOverride(String Addr)
-        {
+        private Boolean SetReg_ProxyOverride(String Addr) {
             Microsoft.Win32.RegistryKey regkey =
                 Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings", true);
             regkey.SetValue("ProxyOverride", Addr, Microsoft.Win32.RegistryValueKind.String);
 
             regkey.Close();
             return true;
+        }
+
+        private void InTasktray()
+        {
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void NetworkChange_NetworkAvailabilityChanged(
+            object sender, System.Net.NetworkInformation.NetworkAvailabilityEventArgs e)
+        {
+            //Invokeが必要か確認し、必要であればInvokeを呼び出す
+            if (this.InvokeRequired)
+            {
+                System.Net.NetworkInformation.NetworkAvailabilityChangedEventHandler dlgt =
+                    new System.Net.NetworkInformation.NetworkAvailabilityChangedEventHandler(
+                        NetworkChange_NetworkAvailabilityChanged);
+                this.Invoke(dlgt, new object[] { sender, e });
+                return;
+            }
+
+            if (e.IsAvailable)
+            {
+                this.Text = "ネットワーク接続が有効になりました。";
+                if (!Debugger.IsAttached)
+                    Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+            }
+
+            else
+            {
+                this.Text = "ネットワーク接続が無効になりました。";
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            Application.Exit();
+        }
+
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
         }
     }
 }
